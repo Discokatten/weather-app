6b535f516d3b597d35fb8c32f4ebc14af0769bef
Discokatten <isabelle.wincrantz@gmail.com>
2026-06-26 14:38:40 +0200

    removed unused files, added eslint wf

--- src/app/components/hero.tsx
+++
@@ -1,9 +0,0 @@
-import WeatherWidget from '@/app/@weather/_components/WeatherWidget'
-
-export default function Hero() {
-  return (
-    <div className="bg-[url(/hero.svg)] bg-no-repeat bg-cover bg-center rounded-2xl h-77 text-white max-w-200">
-      <WeatherWidget />
-    </div>
-  )
-}
