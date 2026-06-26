d392a4a54ce217b489da285a0c2edc21e3e49dd1
Discokatten <isabelle.wincrantz@gmail.com>
2026-06-26 21:45:58 +0200

    fixed wrong heading on edit profile

--- src/app/settings/profile/[id]/page.tsx
+++ src/app/settings/profile/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function EditProfilePage({
   return (
     <main className='w-full max-w-screen-2xl mx-auto px-4 py-10'>
       <section className='bg-theme-900 rounded-3xl border border-theme-700 p-6 shadow-xl'>
-        <SettingsHeader title='Redigera plagg' />
+        <SettingsHeader title='Redigera användare' />
         <div className='mt-8'>
           <EditProfile profile={profile} />
         </div>
