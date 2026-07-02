1d8801ba9a90ad252d98bd7864e701616b6b4b0a
Discokatten <isabelle.wincrantz@gmail.com>
2026-07-02 10:00:10 +0200

    confirm delete on profile and settings

--- src/app/components/Confirm.tsx
+++ src/app/components/Confirm.tsx
@@ -7,14 +7,19 @@ export function Confirm({
 }) {
   return (
     <div
-      className='flex items-center justify-center bg-black/50'
+      className='flex items-center text-xl justify-center rounded-md'
       onClick={(e) => e.target === e.currentTarget && onCancel()}
     >
-      <div className='bg-white dark:bg-neutral-900 rounded-xl p-6 w-full max-w-sm shadow-lg'>
+      <div className='bg-theme-900 rounded-xl p-6 w-full max-w-sm shadow-lg'>
         <p> Är du säker? </p>
-        <div className='flex gap-2 justify-end'>
-          <button onClick={onCancel}>Cancel</button>
-          <button onClick={onConfirm} className='text-red-600'>
+        <div className='flex gap-4 justify-end'>
+          <button onClick={onCancel} className=' hover:text-theme-300'>
+            Ångra
+          </button>
+          <button
+            onClick={onConfirm}
+            className='text-red-600 hover:text-red-800'
+          >
             Ta bort
           </button>
         </div>
